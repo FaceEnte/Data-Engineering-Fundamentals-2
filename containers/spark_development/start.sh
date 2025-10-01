78e6b17980df62c8b43a06ccf59dfe45eb7d5dad
Denis Andrei Szabo <denisandrei.szabo@gmail.com>
2025-10-01 17:13:01 +0200

    Exercise 2
    Solved exercise 2 (spark_rdd) and also renamed the folder from container
    to containers

--- containers/spark_development/start.sh
+++ containers/spark_development/start.sh
@@ -1,17 +1,19 @@
 # start jupyter-pyspark container
 container run -d --name jupyter-pyspark --rm -p 8888:8888 -p 4040:4040 \
     -v $(pwd)/jupyter:/home/jovyan/work \
+    -e CHOWN_EXTRA="/home/jovyan/work" \
+    -e CHOWN_EXTRA_OPTS="-R" \
     quay.io/jupyter/pyspark-notebook:aarch64-spark-3.5.3
 
 # wait until the container has started
-sleep 5
+sleep 3
 
 # run setup commands
 container exec jupyter-pyspark mkdir /tmp/spark-events
 container exec jupyter-pyspark chmod -R go+rwx /tmp/spark-events
 
 # wait until the folders are created and configured
-sleep 5
+sleep 2
 
 # show logs
 container logs -f jupyter-pyspark
---
+++ containers/spark_rdd/start.sh
@@ -0,0 +1,21 @@
+# start jupyter-pyspark container
+container run -d --name jupyter-pyspark-rdd --rm -p 8888:8888 -p 4040:4040 \
+    -v $(pwd)/jupyter:/home/jovyan/work \
+    -v $(pwd)/data:/home/jovyan/work/data \
+    -e CHOWN_EXTRA="/home/jovyan/work" \
+    -e CHOWN_EXTRA="/home/jovyan/work/data" \
+    -e CHOWN_EXTRA_OPTS="-R" \
+    quay.io/jupyter/pyspark-notebook:aarch64-spark-3.5.3
+
+# wait until the container has started
+sleep 3
+
+# run setup commands
+container exec jupyter-pyspark-rdd mkdir /tmp/spark-events
+container exec jupyter-pyspark-rdd chmod -R go+rwx /tmp/spark-events
+
+# wait until the folders are created and configured
+sleep 2
+
+# show logs
+container logs -f jupyter-pyspark-rdd
